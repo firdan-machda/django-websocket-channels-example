6d011f98f5d36353a0ff55754ce9667c4294d46b
firdan <onpeci@gmail.com>
2025-01-30 18:09:05 +0700

    update: add admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ChatSession, ChatUser, ChatMessage
 
 # Register your models here.
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    list_display=['session_uuid', 'created_at']
+
+
+@admin.register(ChatUser)
+class ChatUserAdmin(admin.ModelAdmin):
+    list_display=['chat_session', 'user', 'joined']
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display=['chat_session','user','created_at']
