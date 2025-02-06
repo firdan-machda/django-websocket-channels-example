50b317f6c536c714e266374cb8cb1439de71fe36
firdan <onpeci@gmail.com>
2025-02-06 10:41:37 +0700

    update admin view

--- signaling/admin.py
+++ signaling/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import WebRTCSession, WebRTCUser, WebRTCOffer
 
 # Register your models here.
+
+
+@admin.register(WebRTCSession)
+class WebRTCSessionAdmin(admin.ModelAdmin):
+    list_display = ('session_uuid', 'alias', 'created_at')
+    search_fields = ('alias',)
+
+
+@admin.register(WebRTCUser)
+class WebRTCUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'joined')
+    search_fields = ('username',)
+
+
+@admin.register(WebRTCOffer)
+class WebRTCOfferAdmin(admin.ModelAdmin):
+    list_display = ('webrtc_session', 'user', 'created_at')
+    search_fields = ('webrtc_session__alias', 'user__username')
